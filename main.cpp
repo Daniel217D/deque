929a8fb8dc620c83409882495524d4af1ed1b2c5
Daniil <daniel217032001@gmail.com>
2020-10-01 00:05:49 +0300

    update example

--- main.cpp
+++ main.cpp
@@ -4,17 +4,54 @@
 using std::cout;
 
 int main() {
-    Deque *d = new Deque();
+    auto *d = new Deque();
+
+    cout << "Push with pop: ";
     d->push(1);
     d->push(2);
     d->push(3);
     d->push(4);
-    d;
+    while(!d->is_empty()) {
+        cout << d->pop() << " ";
+    }
+    cout << "\n";
+
+    cout << "Shift with unshift: ";
+    d->shift(1);
+    d->shift(2);
+    d->shift(3);
+    d->shift(4);
     while(!d->is_empty()) {
         cout << d->unshift() << " ";
     }
-
     cout << "\n";
 
+    cout << "Push with unshift: ";
+    d->push(1);
+    d->push(2);
+    d->push(3);
+    d->push(4);
+    while(!d->is_empty()) {
+        cout << d->unshift() << " ";
+    }
+    cout << "\n";
+
+    cout << "Shift with pop: ";
+    d->shift(1);
+    d->shift(2);
+    d->shift(3);
+    d->shift(4);
+    while(!d->is_empty()) {
+        cout << d->pop() << " ";
+    }
+    cout << "\n";
+
+    try {
+        cout << d->pop();
+    } catch (const std::exception& e) {
+        cout << e.what();
+    }
+
+    delete d;
     return 0;
 }
