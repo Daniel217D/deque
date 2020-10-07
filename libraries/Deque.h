f2ec5160811620a03e304f52884f7f5037e30a32
Daniil <daniel217032001@gmail.com>
2020-10-07 22:05:43 +0300

    Add template to deque

--- libraries/Deque.h
+++ libraries/Deque.h
@@ -2,40 +2,109 @@
 
 #include <iostream>
 
+using std::cout;
 using std::string;
+using std::ifstream;
+using std::ofstream;
+using std::out_of_range;
 
+template<class Type>
 class Node {
 private:
-    int value;
+    Type value;
 
-    Node *next;
+    Node<Type> *next;
 
-    friend class Deque;
+    template<typename> friend
+    class Deque;
 
 public:
-    explicit Node(int _value, Node *_next);
+    Node(Type _value, Node *_next = nullptr) : value(_value), next(_next) {
+    }
 
-    ~Node();
+    ~Node() {
+        delete next;
+    }
 
-    Node *get_next();
+    Node *get_next() {
+        return next;
+    }
 
-    int get_value();
+    Type get_value() {
+        return value;
+    }
 };
 
+template<class Type>
 class Deque {
 private:
-    Node *node = nullptr;
+    Node<Type> *node = nullptr;
 
 public:
-    ~Deque();
+    ~Deque() {
+        delete node;
+    }
 
-    bool is_empty();
+    bool is_empty() {
+        return node == nullptr;
+    }
 
-    void push(int value);
+    void push(Type value) {
+        if (is_empty()) {
+            shift(value);
+        } else {
+            Node<Type> *current = node;
 
-    int pop();
+            while (current->next) {
+                current = current->next;
+            }
 
-    void shift(int value);
+            current->next = new Node<Type>(value);
+        }
+    }
 
-    int unshift();
+    Type pop() {
+        Type value;
+
+        if (is_empty()) {
+            throw out_of_range("Deque is empty");
+        } else if (node->next == nullptr) {
+            value = node->value;
+
+            node = nullptr;
+            delete node;
+        } else {
+            Node<Type> *current = node;
+
+            while (current->next->next) {
+                current = current->next;
+            }
+
+            value = current->next->value;
+
+            delete current->next;
+            current->next = nullptr;
+        }
+
+        return value;
+    }
+
+    void shift(Type value) {
+        node = new Node<Type>(value, node);
+    }
+
+    Type unshift() {
+        if (is_empty()) {
+            throw out_of_range("Deque is empty");
+        } else {
+            Type value = node->value;
+            Node<Type> *temp = node;
+
+            node = node->next;
+            temp->next = nullptr;
+            delete temp;
+
+            return value;
+        }
+    }
 };
